60dc5564d23c2c00e575cc39f2a15f76430c6b29
JEFFREY ALBERTO CORTES CUESTA <jeffcorcue@colsubsidio.com>
2025-10-29 21:07:18 -0500

    Se crea ResponseApi como dto de respuesta estándar del microservicio

---
+++ src/main/java/com/dian/ResponseApi.java
@@ -0,0 +1,25 @@
+package com.dian;
+
+import lombok.Data;
+
+import java.time.Instant;
+
+@Data
+public class ResponseApi {
+    private String mensaje;
+    private String codigo;
+    private String timestamp;
+    private String path;
+    private int status;
+    private boolean succes;
+    private ResponseApiError error;
+
+    public ResponseApi() {}
+
+    public ResponseApi(String mensaje, String codigo) {
+        this.mensaje = mensaje;
+        this.codigo = codigo;
+        this.timestamp = Instant.now().toString();
+    }
+}
+
